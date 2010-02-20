48ee9c4b180e5ae8a3e749170501e5bb98889ba0
Haibao Tang <tanghaibao@gmail.com>
2010-02-20 01:22:19 -0800

    none

--- filter_blast.py
+++ filter_blast.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 
 """
 Filter the 3-column BLAST output according to following rules
@@ -15,29 +13,40 @@ import sys
 
 if __name__ == '__main__':
         
+    from optparse import OptionParser
+
+    usage = "%prog infile outfile"
+    parser = OptionParser(usage)
+    (options, args) = parser.parse_args()
+
     try:
-        infile = sys.argv[1]
-        outfile = sys.argv[2]
+        infile = args[0]
+        outfile = args[1]
     except:
-        print "Usage: python filter_blast.py infile outfile"
+        sys.exit(parser.print_help())
+    
+    assert os.path.exists(infile)
 
     fp = file(infile)
     pairs = {}
     j = 0
     for row in fp:
-        j+=1
-        a,b,e = row.split()
+        j += 1
+        a, b, e = row.split()
         e = float(e)
         if a==b: continue
-        if a>b: a,b=b,a
-        pair_name = "%s&%s"%(a,b)
+        if a > b: a, b = b, a
+        pair_name = (a, b)
         if pair_name not in pairs or (pair_name in pairs and e<pairs[pair_name]): 
             pairs[pair_name] = e
 
-    print j,"records read"
-    print len(pairs),"records after filtering"
+    print >>sys.stderr, j, "records read"
+    print >>sys.stderr, len(pairs), "records after filtering"
 
     fw = file(outfile,"w")
-    for k,e in sorted(pairs.items()):
-        a,b = k.split("&")
-        fw.write("%s\t%s\t%g\n"%(a,b,e))
+    for k, e in sorted(pairs.items()):
+        a, b = k
+        fw.write("%s\t%s\t%g\n" % (a, b, e))
+
+    fp.close()
+    fw.close()
